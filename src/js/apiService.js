1c6ef6a0e08f26ab82298a5373a19df4102ccb86
Natalia-doobro <79019782+Natalia-doobro@users.noreply.github.com>
2021-09-02 20:32:54 +0300

    правки ментора

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -7,8 +7,11 @@ const myKey = '23116655-204395131977af9122e6962dd';
 async function fetchPhotoAPI(text, page) {
     const result = await fetch(`${BASE_URL}/?image_type=photo&orientation=horizontal&q=${text}&page=${page}&per_page=12&key=${myKey}`);
     const response = await result.json();
-    return response;
+    return response;  
+    
+        
 }
+    
 
 
 export default { fetchPhotoAPI };
