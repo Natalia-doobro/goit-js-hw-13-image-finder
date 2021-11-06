eea6e6f199c291c0ff938e617ed40f3c045d9774
Natalia-doobro <79019782+Natalia-doobro@users.noreply.github.com>
2021-11-06 23:15:00 +0200

    fix

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -4,7 +4,7 @@ const myKey = '23116655-204395131977af9122e6962dd';
 
 async function fetchPhotoAPI(text, page) {
     
-    const result = await fetch(`${BASE_URL}/?image_type=photo&orientation=horizontal&q=${text}&page=${page}&per_page=12&key=${myKey}`);
+    const result = await fetch(`${BASE_URL}/?image_type=photo&orientation=horizontal&q=${text}&page=${page}&per_page=15&key=${myKey}`);
     const response = await result.json();
 
     return response;  
